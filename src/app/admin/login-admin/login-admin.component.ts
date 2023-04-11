1358cf3cd789bbefa751451108988b61501e94fa
argl <alejandro02262@gmail.com>
2023-04-11 17:26:48 -0600

    cambios

--- src/app/admin/login-admin/login-admin.component.ts
+++ src/app/admin/login-admin/login-admin.component.ts
@@ -9,12 +9,11 @@ import { Component, OnInit } from '@angular/core';
 import { emailValidator } from 'src/app/theme/utils/app-validators';
 
 @Component({
-  selector: 'app-login-admin',
-  templateUrl: './login-admin.component.html',
-  styleUrls: ['./login-admin.component.scss']
+  selector: "app-login-admin",
+  templateUrl: "./login-admin.component.html",
+  styleUrls: ["./login-admin.component.scss"],
 })
 export class LoginAdminComponent implements OnInit {
-
   loginForm: FormGroup;
 
   registerForm: FormGroup;
@@ -24,49 +23,64 @@ export class LoginAdminComponent implements OnInit {
     public formBuilder: FormBuilder,
     public router: Router,
     public snackBar: MatSnackBar,
-    private usuarioService:UsuariosService,
-    private sessionStorage:SessionAdminStorageService
-    ) {}
+    private usuarioService: UsuariosService,
+    private sessionStorage: SessionAdminStorageService
+  ) {}
 
   ngOnInit() {
     this.loginForm = this.formBuilder.group({
-      'email': ['', Validators.compose([Validators.required, emailValidator])],
-      'password': ['', Validators.compose([Validators.required, Validators.minLength(6)])]
+      email: ["", Validators.compose([Validators.required, emailValidator])],
+      password: [
+        "",
+        Validators.compose([Validators.required, Validators.minLength(6)]),
+      ],
     });
     this.registerForm = this.formBuilder.group({
-      'correoElectronico': ['', Validators.compose([Validators.required, emailValidator])],
-      'password': ['', Validators.compose([Validators.required, Validators.minLength(6)])],
-      'nombre': ['', Validators.compose([Validators.required])],
-      'apellidoPaterno': ['', Validators.compose([Validators.required])],
-      'apellidoMaterno': ['', Validators.compose([Validators.required])],
-      'telefono': ['', Validators.compose([Validators.required])],
-      'estatus': [0, Validators.compose([Validators.required])],
-      'multipartFile': [null],
-      'bytesImagen': [null],
+      correoElectronico: [
+        "",
+        Validators.compose([Validators.required, emailValidator]),
+      ],
+      password: [
+        "",
+        Validators.compose([Validators.required, Validators.minLength(6)]),
+      ],
+      nombre: ["", Validators.compose([Validators.required])],
+      apellidoPaterno: ["", Validators.compose([Validators.required])],
+      apellidoMaterno: ["", Validators.compose([Validators.required])],
+      telefono: ["", Validators.compose([Validators.required])],
+      estatus: [0, Validators.compose([Validators.required])],
+      multipartFile: [null],
+      bytesImagen: [null],
     });
   }
 
-  public onLoginFormSubmit(values: Object): void {
+  onLoginFormSubmit() {
     if (this.loginForm.valid) {
-
-      let usuarioLogin:Usuario={
-        correoElectronico:this.loginForm.get("email").value,
-        password: this.loginForm.get("password").value
+      let usuarioLogin: Usuario = {
+        correoElectronico: this.loginForm.get("email").value,
+        password: this.loginForm.get("password").value,
       };
-      // usuarioLogin.correoElectronico=this.loginForm.get("email").value;
-      // usuarioLogin.password=this.loginForm.get("password").value;
 
-      this.usuarioService.loginUsuario(usuarioLogin).subscribe({next: data=>{
-        if(data.ok){
-          this.sessionStorage.saveUser(data.response);
-          this.router.navigate(['/admin']);
-        }else{
-          Util.errorMessage(data.mensaje);
-        }
-      },error:error=>{
-        Util.errorMessage(error.error.mensaje);
-      }})
+      this.usuarioService.loginUsuario(usuarioLogin).subscribe((data) => {
+        console.log(data);
+      });
+
+      this.usuarioService.loginUsuario2(usuarioLogin).subscribe({
+        next: (data) => {
+          if (data.ok) {
+            this.sessionStorage.saveUser(data.response);
+            this.router.navigate(["/admin"]);
+          } else {
+            Util.errorMessage(data.mensaje);
+          }
+          const token = localStorage.getItem("access_token");
+          console.log(token);
+          console.log(data);
+        },
+        error: (error) => {
+          Util.errorMessage(error.error.mensaje);
+        },
+      });
     }
   }
-
 }
